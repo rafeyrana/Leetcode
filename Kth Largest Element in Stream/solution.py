8a192b6f28de7a4a026662b49d96b6cbdaf335de
Rafey Rana <24100103@lums.edu.pk>
2024-11-06 12:58:49 -0500

    kth largest element in stream

---
+++ Kth Largest Element in Stream/solution.py
@@ -0,0 +1,22 @@
+from collections import Counter
+import heapq
+class KthLargest:
+    '''
+    use a min heap to keep track of the k largest elements. so in this case when you pop it will be the kth largest element
+    when adding add to the heap, readjust the heap to keep it at k elements. and then peek at the top of the heap to get the kth largest element
+    '''
+    def __init__(self, k: int, nums: List[int]):
+        self.heap , self.k = nums, k
+        heapq.heapify(self.heap)
+        while len(self.heap) > k:
+            heapq.heappop(self.heap)
+
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+        while len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
+
+
+        
