b36bec4c37ca5ace62e93691ed198a3e1f11010e
Rafey Rana <24100103@lums.edu.pk>
2025-03-24 23:52:50 -0400

    longest consecutive sequence

--- Longest Consecutive Sequence/solution.py
+++ Longest Consecutive Sequence/solution.py
@@ -1,13 +1,17 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        nums_set = set(nums)
+        # using a set for O(1) look up in the nums array, convert the nums to a set for faster search
+        checker = set(nums)
         longest = 0
-        for k in nums:
-            length = 0
-            if k - 1 not in nums_set:
-                length += 1
-                while k + length in nums_set:
-                    length += 1
-                longest = max(length, longest)
+        for num in nums:
+            if num - 1 not in checker: # this means this is the start of the sequence
+                temp = num
+                local_longest = 1
+                while temp + 1 in nums:
+                    local_longest += 1
+                    temp = temp + 1
+                longest = max(longest, local_longest)
         return longest
-                
+
+
+    
