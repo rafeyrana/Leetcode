2efe4e64f525b827d13616d49ab880251294af41
Rafey Rana <24100103@lums.edu.pk>
2025-03-26 00:06:29 -0400

    Max Water in Container

--- Container with Most water/solution.py
+++ Container with Most water/solution.py
@@ -1,21 +1,30 @@
 class Solution:
     def maxArea(self, heights: List[int]) -> int:
-        # first lets do the naive solution
-        start_counter = 0
-        end_counter = len(heights) -1
-        max_area= 0
-        while start_counter != end_counter and start_counter < len(heights) :
-            print(start_counter, end_counter)
-            area = min(heights[start_counter], heights[end_counter]) * abs(start_counter - end_counter)
-            if area > max_area:
-                max_area = area 
-            if heights[start_counter] < heights[end_counter]:
-                start_counter += 1
-            elif heights[start_counter] > heights[end_counter]:
-                end_counter -= 1
-            else:
-                start_counter += 1
-                end_counter -= 1
+        '''
+        1. what does amount of water mean here? is it the area?
+            how do we calculate the area? index diff of the two bars and the min of their heights?
 
-        return max_area
+
+
+        the brute force approach is to check each and every pair given here to check which of them is the largest in terms of the areas
+        or we can use a two pointer approach because we are exploring a linear structure and need to optimise for the largest area
+        so the two pointers will start at opposite sides and then converge towards the middle
+        but how do we move the pointers until they cross
+        since the bounding factor for the area calculation is the min of the heights we need to change that to get a better area so we will move the element with the smaller height 
+        '''
+        left_pointer = 0 
+        right_pointer = len(heights) - 1
+        max_water_area = 0
+        
+        while left_pointer < right_pointer:
+            current_area = min(heights[left_pointer], heights[right_pointer]) * (right_pointer - left_pointer)
+            max_water_area = max(current_area, max_water_area)
+            
+            if heights[left_pointer] > heights[right_pointer]:
+                right_pointer -= 1
+            else:
+                # if less than or equal so we choose to move the left pointer if they are equal
+                left_pointer += 1
+                
+        return max_water_area
         
