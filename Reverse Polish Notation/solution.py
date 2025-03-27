a3b8e99e8f6799737e3209a18f19c25c5919f1b6
Rafey Rana <24100103@lums.edu.pk>
2025-03-26 22:51:29 -0400

    Reverse Polish Notation

--- Reverse Polish Notation/solution.py
+++ Reverse Polish Notation/solution.py
@@ -1,18 +1,26 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
+        # using a stack to store numbers until we encounter an operand
+        # pop last two values and perform the operation and push the result back on to the stack until we are donw
+        # assume this will always be valid and will always evaluate to one single solution which we can return
         stack = []
-        for x in tokens:
-            if x not in ["+", "-", "*", "/"]:
-                stack.append(int(x))
-            else:
-                num2 = stack.pop()
-                num1 = stack.pop()
-                if x == "+":
-                    stack.append(num1 + num2)
-                elif x == "-":
-                    stack.append(num1 - num2)
-                elif x == "*":
-                    stack.append(num1 * num2)
-                elif x == "/":
-                    stack.append(int(num1 / num2))
-        return stack.pop()
+        for token in tokens:
+            match token:
+                case "+":
+                    stack.append(stack.pop() + stack.pop())
+                case "-":
+                    first_pop = stack.pop()
+                    second_pop = stack.pop()
+                    stack.append(second_pop - first_pop)
+                case "*":
+                    stack.append(stack.pop() * stack.pop())
+                case "/":
+                    first_pop = stack.pop()
+                    second_pop = stack.pop()
+                    stack.append(int(second_pop / first_pop))
+                case _:
+                    stack.append(int(token))
+        return stack[-1]
+
+                
+        
