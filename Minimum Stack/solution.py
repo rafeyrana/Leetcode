898900be56f588916647ad7de0b04e21696781c5
Rafey Rana <24100103@lums.edu.pk>
2025-03-26 22:39:26 -0400

    space optimisation

--- Minimum Stack/solution.py
+++ Minimum Stack/solution.py
@@ -4,6 +4,7 @@ class MinStack:
     1. One for the value that we have to add to the stack
     2. Min stack for keeping track of the minimum value uptil this point
     performs all operations in O(1)
+    only add to the min stack if it is smaller than the last min or else no point in adding as it is not going to be a min
     
     '''
 
@@ -14,15 +15,14 @@ class MinStack:
 
     def push(self, val: int) -> None:
         self.main_stack.append(val)
-        if not self.min_stack:
+        if not self.min_stack or val <= self.min_stack[-1]:
             self.min_stack.append(val)
-        else:
-            self.min_stack.append(min(val, self.min_stack[-1]))
         
 
     def pop(self) -> None:
-        self.main_stack.pop()
-        self.min_stack.pop()
+        val = self.main_stack.pop()
+        if val == self.min_stack[-1]:
+            self.min_stack.pop()
         
 
     def top(self) -> int:
