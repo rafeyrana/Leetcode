0b6bdaa50058fae2859c3750fd30f092153b9061
Rafey Rana <24100103@lums.edu.pk>
2025-03-26 22:23:48 -0400

    valid paranthesis

--- Valid Parenthesis String/solution.py
+++ Valid Parenthesis String/solution.py
@@ -1,27 +1,22 @@
 class Solution:
-    def checkValidString(self, s: str) -> bool:
+    def isValid(self, s: str) -> bool:
         '''
-        the intuition is to go over the string in one pass. we will keep a track of the possible values of the open paranethesis count using the min and max algo such as the * will either change it by -1, 0, +1 and then we can maintain the range
-        if the value ever goes to -1 we will not keep it and we will make it 0
-        check for another case that if the max_open is ever less than 0 then the order is not correct and we have to return false for this
-        in the end if the min is zero that means that at some point while iterating and considering all possible combos they combined to sum up to 0 meaning the string has been balanced
+        so if we have to go through the string of paranthesis going in order
+        the last open paranthesis has to be the first to be closed and the second and then so on
+        this points to Last In First Out Approach we will need to use a stack to keep a track of the opening brackets and when we encounter a closing bracket we pop from the stack and check if its the corresponding bracket or not
+        early return False and return True at the end
         '''
-        min_open = max_open = 0
-        for c in s:
-            if c == "(":
-                min_open += 1
-                max_open += 1
-            elif c == ")":
-                min_open -= 1
-                max_open -= 1
-            else:
-                min_open = min_open - 1 # in this case the minimum value can be in the case of it being )
-                max_open = max_open + 1
-            if min_open < 0:
-                min_open = 0
-            if max_open < 0:
+        stack = []
+        parans = {
+            ")" : "(",
+            "}" : "{",
+            "]" : "["
+        }
+        for char in s:
+            if char not in parans:
+                stack.append(char)
+            elif not stack or stack.pop() != parans[char]:
                 return False
-        # now that we have traversed the whole array we can check if at any point we had that balanced or not
-        return min_open == 0
 
+        return not stack # stack should be empty
         
